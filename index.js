ccaca15435da40cc6771ec199eeb1931dca792ae
Мирзазаде Эмиль Алибей <Mirza_bs61@itstep.edu.az>
2023-10-26 20:19:24 +0400

    3nd point of HW is done

--- index.js
+++ index.js
@@ -2,14 +2,24 @@
 
 console.clear();
 
-function timer(sec,startTime){
-    let ms=sec*1000;
-    let cnt=0;
-    let interval=setInterval(function () {
-        if(startTime===0)clearInterval(interval);
-        console.log(startTime--);
-    },ms);
+function timer(time){
+    let currentTime=new Date();
+    let endTime=new Date();
+    let timeArr=time.split(':');
+    for(let i of timeArr){
+        if(i.split('').length!==2)throw new Error('not correct time format');
+    }
+    let hour=timeArr[0];
+    let minute=timeArr[1];
+    let second=timeArr[2];
+    endTime.setHours(hour);
+    endTime.setMinutes(minute);
+    endTime.setSeconds(second);
+
+    setTimeout(function () {
+        console.log("Time's up!");
+    },endTime-currentTime);
 
 }
 
-timer(2,5);
+timer('20:17:00');
